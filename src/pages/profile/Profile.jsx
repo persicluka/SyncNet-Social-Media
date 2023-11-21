b85dacab4cb6eab865b3fa85bad0de92956b4de3
persicluka <luka.persic@hotmail.com>
2023-11-21 04:48:14 +0100

    Profile Page

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "./profile.scss";
+import { AuthContext } from "../../context/AuthContext";
 
 function Profile() {
+  const { currentUser } = useContext(AuthContext);
+
   return (
     <div className="profile">
-      <img src="" alt="" />
-      <hr />
+      <div className="images">
+        <img src={currentUser.image} alt="" />
+        <img src={currentUser.image} alt="" className="profilePic" />
+      </div>
+      <div className="profile">
+        <div className="userInfo">
+          <span>{currentUser.name}</span>
+        </div>
+      </div>
     </div>
   );
 }
